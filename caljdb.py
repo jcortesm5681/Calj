724750810a9f7ebe05bfc4975a9967437b17cee7
jcortesm5681 <105297899+jcortesm5681@users.noreply.github.com>
2023-08-03 10:26:30 -0500

    Update caljdb.py

--- caljdb.py
+++ caljdb.py
@@ -1,33 +1,23 @@
 import sqlite3  , pandas
-#import os
+###
+#Este programa carga en la base de datos sqlite los festivos de colombia que estan almacenados en la ruta del archivo plano en GITHUB
+#"https://raw.githubusercontent.com/jcortesm5681/Festivus/main/festivus.ini"
+# usa la libreria PANDAS para cargar facilmente de CSV a la BD dicha.
 
+## selecconar ruta del archivo donde se va a guardar la BD, tiene que ser el mismo que en calj sinux o max puede ser 
 
-#wd = os.getcwd()
-#print("DB directory is ", wd+"\Festivus.db")
-
-#con = sqlite3.connect( wd+"\Festivus.db")# no sirve bien
-con = sqlite3.connect("D:/dist/Festivus.db")
-
+con = sqlite3.connect("D:/dist/Festivus.db") # rutas en windows
 cur = con.cursor()
-
 
 # Create table
 # Insert a row of data
-
-
 df = pandas.read_csv("https://raw.githubusercontent.com/jcortesm5681/Festivus/main/festivus.ini")
 df.to_sql("FESTIVUS", con, if_exists='replace', index=False,method="multi")
-
-
 # Save (commit) the changes
 con.commit()
-
-
-
+# validar si el contenido en la BD quedó bien guardado. 
 cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
 print(cur.fetchall())
-
-
 cur.execute("select FEST from FESTIVUS")
 print(cur.fetchall())
 

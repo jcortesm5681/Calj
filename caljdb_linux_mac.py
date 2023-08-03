99dab2d01440e90e021099f833a05eae5f123155
jcortesm5681 <105297899+jcortesm5681@users.noreply.github.com>
2023-08-03 10:31:10 -0500

    Update caljdb_linux_mac.py

--- caljdb_linux_mac.py
+++ caljdb_linux_mac.py
@@ -1,30 +1,26 @@
 import sqlite  , pandas
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+###
+#Este programa carga en la base de datos sqlite los festivos de colombia que estan almacenados en la ruta del archivo plano en GITHUB
+#"https://raw.githubusercontent.com/jcortesm5681/Festivus/main/festivus.ini"
+# usa la libreria PANDAS para cargar facilmente de CSV a la BD dicha.
+#Para linux o mac fue necesario importar y crear ssl
+
+## selecconar ruta del archivo donde se va a guardar la BD, tiene que ser el mismo que en calj sinux o max puede ser 
 
 con = sqlite3.connect("/Users/jac/dist/Festivus.db")
-
 cur = con.cursor()
-
 
 # Create table
 # Insert a row of data
-
-
 df = pandas.read_csv("https://raw.githubusercontent.com/jcortesm5681/Festivus/main/festivus.ini")
-print(df)
 df.to_sql("FESTIVUS", con, if_exists='replace', index=False)
-
-
 # Save (commit) the changes
 con.commit()
-
-
-
+# validar si el contenido en la BD quedó bien guardado. 
 cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
 print(cur.fetchall())
-
-
 cur.execute("select FEST from FESTIVUS")
 print(cur.fetchall())
 
